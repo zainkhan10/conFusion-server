89df85629cfc780e95c8a81880b8b45618209ab6
Zain Khan <zain.khaan147@gmail.com>
2020-10-17 17:16:57 +0500

    Basic Authentication

--- app.js
+++ app.js
@@ -1,45 +1,79 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var dishRouter = require("./routes/dishRouter");
+var promoRouter = require("./routes/promoRouter");
+var leaderRouter = require("./routes/leaderRouter");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Dishes = require('./models/dishes');
-const Promotions = require('./models/promotions');
-const Leaders = require('./models/leaders');
+const Dishes = require("./models/dishes");
+const Promotions = require("./models/promotions");
+const Leaders = require("./models/leaders");
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
-connect.then((db) => {
-  console.log('Connected correctly to the server');
-}, (err) => console.log(err))
+connect.then(
+  (db) => {
+    console.log("Connected correctly to the server");
+  },
+  (err) => console.log(err)
+);
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
+function auth(req, res, next) {
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+
+  var auth = new Buffer(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  var username = auth[0];
+  var password = auth[1];
+
+  if (username === "admin" && password === "password") {
+    next(); 
+  } else {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -50,11 +84,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+// mongod --dbpath=data --bind_ip 127.0.0.1
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
